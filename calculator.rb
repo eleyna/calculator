fc2d27fcdcfea38079c3538983be3e127b356d31
eleyna <eleyna@gmail.com>
2013-06-18 17:01:36 -0400

    all 8 functions now included

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,5 @@
+PI = 3.14159265359
+
 def calculator(calc)
   case calc
     when "addition"
@@ -12,6 +14,18 @@ def calculator(calc)
     when "division"
       x, y = get_two_numbers
       div(x, y)
+    when "sphere volume"
+      x = get_one_number
+      volume_of_sphere(x)
+    when "area"
+      x, y = get_two_numbers
+      area(x, y)
+    when "volume"
+      x, y, z = get_three_numbers
+      volume(x, y, z)
+    when "circle area"
+      x = get_one_number
+      area_of_circle(x)
     else
       "You did not enter a calculation"
   end
@@ -29,12 +43,33 @@ def mult(num_1, num_2)
   return num_1 * num_2
 end
 
+def volume_of_sphere(radius)
+  return (4/3.0 * PI * (radius**3))
+end
+
+def area_of_circle(radius)
+  return (PI * radius**2)
+end
+
+def area(width, length)
+  return width * length
+end
+
+def volume(width, length, height)
+  return width * length * height
+end
+
 def div(num_1, num_2)
   while (num_2 == 0)
     puts "\nCannot divide by 0. Please enter another number:"
     num_2 = gets.chomp.to_f
   end
   return num_1 / num_2
+end
+
+def get_one_number
+  puts "\nPlease enter the number:"
+  return gets.chomp.to_f
 end
 
 def get_two_numbers
@@ -51,7 +86,7 @@ def get_three_numbers
   puts "\nPlease enter the second number:"
   second = gets.chomp.to_f
   puts "\nPlease enter the third number:"
-  third = get.chomp.to_f
+  third = gets.chomp.to_f
   return first, second, third
 end
 
@@ -61,7 +96,8 @@ answer = gets.chomp
 
 while (answer == "yes")
   puts "\nWhat type of calculation would you like to perform?"
-  puts "(addition, subtraction, multiplication, division)"
+  puts "(addition, subtraction, multiplication, division, " +
+       "sphere volume, area, volume, circle area)"
   calc = gets.chomp
   puts "\nYour result is: " + calculator(calc).to_s
   puts "\nWould you like to perform another calculation? (yes/no)\n"
